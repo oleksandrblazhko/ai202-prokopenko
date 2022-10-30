9e4b2d609849d4fb537aa177a210b1a087935af9
Прокопенко Валерій <101650964+VProkop28@users.noreply.github.com>
2022-10-30 21:30:36 +0200

    Update 5- Program-Methods-Using-Example.js

--- 3-SoftwareConstruction/5- Program-Methods-Using-Example.js
+++ 3-SoftwareConstruction/5- Program-Methods-Using-Example.js
@@ -1,68 +1,51 @@
-const ProfileScreen = () => {
-  window.scrollTo(0, 0);
+/* Приклад використання методів програмних классів  */
+/* Перегляд даних користувача (робота з класом user)*/
+export const getUserDetails = (id) => async(dispatch, getState) =>{
+    try {
+        dispatch({type: USER_DETAILS_REQUEST});
+    const {
+        userLogin : {userInfo},
+    } = getState();
 
-const dispatch = useDispatch()
-
-const userLogin = useSelector((state) => state.userLogin)
-const { userInfo } = userLogin;
-const orderListMy = useSelector((state) => state.orderListMy)
-const { loading, error, tracks } = orderListMy;
-
-  return (
-                </div>
-                <div className="author-card-details col-md-7">
-                  <h5 className="author-card-name mb-2">
-                    <strong>{userInfo.name}</strong>
-                  </h5>
-                  <span className="author-card-position">
-                    <>Joined {moment(userInfo.createdAt).format('LL')}</>
-                  </span>
-                </div>
-              </div>
-            </div>
-            <div className="wizard pt-3 ">
-              <div class="d-flex align-items-start">
-                <div
-                  class="nav align-items-start flex-column col-12 nav-pills me-3 "
-                  id="v-pills-tab"
-                  role="tablist"
-                  aria-orientation="vertical"
-                >
-                  <button
-                    class="nav-link active"
-                    id="v-pills-home-tab"
-                    data-bs-toggle="pill"
-                    data-bs-target="#v-pills-home"
-                    type="button"
-                    role="tab"
-                    aria-controls="v-pills-home"
-                    aria-selected="true"
-                  >
-                    Profile Settings
-                  </button>
-                  <button
-                    class="nav-link d-flex justify-content-between"
-                    id="v-pills-profile-tab"
-                    data-bs-toggle="pill"
-                    data-bs-target="#v-pills-profile"
-                    type="button"
-                    role="tab"
-                    aria-controls="v-pills-profile"
-                    aria-selected="false"
-                  >
-                    Orders List
-                    <span className="badge2">{orders ? orders.length : 0}</span>
-                  </button>
-
-          {/* panels */}
-
-              <Orders orders={orders} loading={loading} error={error}/>
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+    const config = {
+        headers:{
+            Authorization:`Bearer ${userInfo.token}`
+        },
+    };
 };
+  
+/* Оновлення даних користувача (робота з класом user)*/
+export const updateUserProfile = (user) => async(dispatch,getState) =>{
+    try {
+        dispatch({type: USER_UPDATE_PROFILE_REQUEST});
+    const {
+        userLogin : {userInfo},
+    } = getState();
 
-export default ProfileScreen;
+    const config = {
+        headers:{
+            "Content-type":"application/json",
+            Authorization:`Bearer ${userInfo.token}`
+        },
+    };
+
+        const {data} = await axios.put(`/api/users/profile`, user, config);
+        dispatch({ type: USER_UPDATE_PROFILE_SUCCESS, payload: data });
+        dispatch({ type: USER_LOGIN_SUCCESS, payload: data });
+
+        localStorage.setItem("userInfo",JSON.stringify(data))
+
+    } catch (error) {
+        const message = 
+        error.response && error.response.data.message 
+            ? error.response.data.message
+            : error.message;
+            if (message === "Not atorized, token failed") {
+                dispatch(logout());
+            }
+        dispatch({     
+            type: USER_UPDATE_PROFILE_FAIL,
+            payload:message,
+        });
+    }
+}
